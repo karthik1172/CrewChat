7e9be74cbc439474f9bbf2ea38c5d3c3334e0bdd
karthik1172 <karthik.p.rashinkar@gmail.com>
2026-01-13 20:12:32 +0530

    Initial Commit

---
+++ CrewChat/CrewChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrewChatApp.swift
+//  CrewChat
+//
+//  Created by Karthik Rashinkar on 13/01/26.
+//
+
+import SwiftUI
+
+@main
+struct CrewChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
